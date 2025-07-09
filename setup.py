aecad8ecbcc2cacd6e59e5cf59ecc05256e1b3ce
V_DeepakGupta <V-Deepak.Gupta@shoplc.com>
2025-07-09 20:29:09 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Prahsant Gupta',
+    author_email= 'imprashant3108@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
